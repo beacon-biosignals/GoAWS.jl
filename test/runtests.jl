077db6042b75f4243a96afd4d802ba1dd8fd2792
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-11-14 19:57:41 +0100

    format

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,24 +46,25 @@ end
         with_go_aws() do aws_config
             result = parse(SQS.create_queue("my_queue"; aws_config))
             queue_url = result["CreateQueueResult"]["QueueUrl"]
-        
+
             ret = parse(SQS.send_message("hello", queue_url; aws_config))
             id = ret["SendMessageResult"]["MessageId"] # looks like a UUID, but isn't documented to be one, so guess we should leave it as a string
-        
-            messages = parse(SQS.receive_message(queue_url, Dict("WaitTimeSeconds" => 1); aws_config))
-        
+
+            messages = parse(SQS.receive_message(queue_url, Dict("WaitTimeSeconds" => 1);
+                                                 aws_config))
+
             @test messages["ReceiveMessageResult"]["Message"]["Body"] == "hello"
             receipt = messages["ReceiveMessageResult"]["Message"]["ReceiptHandle"]
             @test startswith(receipt, id)
             SQS.delete_message(queue_url, receipt; aws_config)
-        
-            SQS.delete_queue(queue_url; aws_config)
+
+            return SQS.delete_queue(queue_url; aws_config)
         end
     end
 
     @testset "GoAWSConfig" begin
         # Can pass strings
-        cfg = GoAWSConfig(; endpoint = "localhost:40912")
+        cfg = GoAWSConfig(; endpoint="localhost:40912")
         @test cfg.endpoint == URI("http://localhost:40912")
     end
 end
