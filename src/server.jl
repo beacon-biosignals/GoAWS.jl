bf0649c159f00a8fbdec404598486924b2c7025d
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-11-15 17:32:19 +0100

    add `GoAWSConfig` constructor, improve coverage

--- src/server.jl
+++ src/server.jl
@@ -125,10 +125,13 @@ function with_go_aws(f; address=DEFAULT_ADDRESS, region="us-east-2", kw...)
     try
         run(server; wait=false)
         sleep(0.5)  # give the server just a bit of time, though it is very fast to start
-        config = GoAWSConfig(address, region)
+        config = GoAWSConfig(server)
         f(config)
     finally
         # Make sure we kill the server even if a test failed.
         kill(server)
     end
 end
+
+# Helper to create a config from a server directly
+GoAWSConfig(s::Server) = GoAWSConfig(; endpoint=s.address, s.region)
