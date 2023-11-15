7d42e8e11174978e2ff7b16b5edc43ab36d697e1
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-11-15 12:51:46 +0100

    format

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,18 @@ using Documenter
 DocMeta.setdocmeta!(GoAWS, :DocTestSetup, :(using GoAWS); recursive=true)
 
 makedocs(;
-    modules=[GoAWS],
-    authors="Beacon Biosignals, Inc.",
-    repo=Remotes.GitHub("beacon-biosignals", "GoAWS.jl"),
-    sitename="GoAWS.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://beacon-biosignals.github.io/GoAWS.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         modules=[GoAWS],
+         authors="Beacon Biosignals, Inc.",
+         repo=Remotes.GitHub("beacon-biosignals", "GoAWS.jl"),
+         sitename="GoAWS.jl",
+         format=Documenter.HTML(;
+                                prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://beacon-biosignals.github.io/GoAWS.jl",
+                                edit_link="main",
+                                assets=String[],),
+         pages=["Home" => "index.md"],)
 
 deploydocs(;
-    repo="github.com/beacon-biosignals/GoAWS.jl.git",
-    devbranch="main",
-    push_preview=true,
-)
+           repo="github.com/beacon-biosignals/GoAWS.jl.git",
+           devbranch="main",
+           push_preview=true,)
