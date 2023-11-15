ec3c446da75b494118ba7b3241e49b130001437d
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-11-15 19:11:14 +0100

    document GoAWSConfig (#6)
    * document GoAWSConfig
    
    * fix syntax highlighting

--- src/aws_interop.jl
+++ src/aws_interop.jl
@@ -1,3 +1,40 @@
+"""
+    GoAWSConfig <: AWS.AbstractAWSConfig
+    GoAWSConfig(; endpoint=GoAWS.DEFAULT_ADDRESS, region="us-east-2")
+    GoAWSConfig(s::Server)
+
+Constructs an `AbstractAWSConfig` for use with AWS.jl, to configure
+SQS and SNS to use GoAWS.
+
+## Examples
+
+Here we demonstrate using the config with a local server we launch
+as a subprocess.
+
+```julia
+using GoAWS, AWS
+@service SQS
+
+server = GoAWS.Server()
+aws_config = GoAWSConfig(server)
+run(server; wait=false)
+
+# ... now we can use the config:
+SQS.create_queue("my_queue"; aws_config)
+
+kill(server) # when you are done
+```
+
+One can also use a `GoAWSConfig` with a GoAWS server launched outside
+of the Julia process. Simply set the `endpoint` and `region` using the
+keyword argument constructor:
+
+```julia
+aws_config = GoAWSConfig(; endpoint="localhost:5203", region="us-east-1") 
+# ... now we can use the config:
+SQS.create_queue("my_queue"; aws_config)
+```
+"""
 struct GoAWSConfig <: AWS.AbstractAWSConfig
     endpoint::URI
     region::String
